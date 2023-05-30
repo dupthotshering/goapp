c4071d187382c83657e70b2d2063bd2d778e32b5
dupthotshering <12220090.gcit@rub.edu.bt>
2023-05-30 10:22:40 +0000

    first commit

--- myapp/datastore/postgres/db.go
+++ myapp/datastore/postgres/db.go
@@ -9,17 +9,17 @@ import (
 )
 
 const (
-	postgres_host     = "db"
+	postgres_host     = "dpg-chqr8lu7avjb90mv1j5g-a.singapore-postgres.render.com"
 	postgres_port     = 5432
-	postgres_user     = "postgres"
-	postgres_password = "postgres"
-	postgres_dbname   = "my_db"
+	postgres_user     = "postgres_admin"
+	postgres_password = "WecY2cM3z9wedEedYhpWPmaLqtXLHWmW"
+	postgres_dbname   = "my_db_xvdh"
 )
 
 var Db *sql.DB
 
 func init() {
-	db_info := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", postgres_host, postgres_port, postgres_user, postgres_password, postgres_dbname)
+	db_info := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s ", postgres_host, postgres_port, postgres_user, postgres_password, postgres_dbname)
 
 	var err error
 	Db, err = sql.Open("postgres", db_info)
